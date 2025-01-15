fff447e1e745ad1c3dac0c2952ac81d4dfb92da8
Diego Escalante <escaland@shernet.sheridancollege.ca>
2025-01-15 11:00:33 -0500

    MAN IDK IM TRYING T KEEP UPPPPPPPP

---
+++ Assets/Script/AnimationCurveDemo.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationCurveDemo : MonoBehaviour
+{
+    public AnimationCurve curve;
+    public float t;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector2(Mathf.Lerp(-4, 5, t), 0);
+    }
+}
