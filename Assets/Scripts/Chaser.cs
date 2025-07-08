dd2490e4ce634fe4d4d57892837f99b23fcc5ff0
Diego(Aria) <deejay.e2@icloud.com>
2025-07-08 18:25:22 -0400

    added the last clicked variable
    added a variable to track the last click of the mouse, this will keep the square moving to the click of the mouse after you click

--- Assets/Scripts/Chaser.cs
+++ Assets/Scripts/Chaser.cs
@@ -12,6 +12,7 @@ public class Chaser : MonoBehaviour
     public float speed;
     bool ifmouseIsPressed;
 
+    Vector3 lastclicked = Vector3.zero;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,20 +23,27 @@ public class Chaser : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+
         Vector3 Mousepositioninworldspace = gamecam.ScreenToWorldPoint(Input.mousePosition);
          Mousepositioninworldspace.z = 0f;
 
-        Vector3 start = transform.position;
-        Vector3 target = Mousepositioninworldspace;
-        //in strad if input.mouse becasue we want in world space or it throws around
-        Vector3 directiontomove = target - start;
+        if(Input.GetMouseButtonDown(0))
+        {
+            lastclicked = Mousepositioninworldspace;
+        }
+
+Vector3 start = transform.position;
+Vector3 target = lastclicked;
+//in strad if input.mouse becasue we want in world space or it throws around
+Vector3 directiontomove = target - start;
 
 ;
-        bool ifRightIsPressed = Input.GetMouseButton(0); 
+        //bool ifRightIsPressed = Input.GetMouseButton(0); 
 
-        if (ifRightIsPressed == true)
+       // if (ifRightIsPressed == true)
         {
-            transform.position = transform.position + directiontomove * speed;
+            //transform.position = transform.position + directiontomove * speed;
         }
     }
 }
